8cc62a443c4670b41f4ed2a940e5405290f5e66c
Astranautical <astranauticalllabs@gmail.com>
2022-05-22 16:37:20 -0400

    Added functionality to add custom ModItems.

--- src/main/java/io/astranautical/mccourse/MCCourseMod.java
+++ src/main/java/io/astranautical/mccourse/MCCourseMod.java
@@ -1,17 +1,27 @@
 package io.astranautical.mccourse;
 
+import io.astranautical.mccourse.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MCCourseMod implements ModInitializer {
-	public static final String MOD_ID = "mccourse";		// MOD_ID can only contain lowercase characters, underscore, dash, and numbers. NO UPPERCASE!!!
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+
+	public static final String MOD_ID = "mccourse";
+	// MOD_ID can ONLY CONTAIN lowercase characters, underscores, dashes, and numbers. NO SPACES!!! NO UPPERCASE!!
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
 
+		ModItems.registerModItems();
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
