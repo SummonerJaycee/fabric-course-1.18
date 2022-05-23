8c1c0abfc02efdbc95b30121606580ce5b9597be
Astranautical <astranauticalllabs@gmail.com>
2022-05-23 17:50:16 -0400

    Added: Speedy Block

--- src/main/java/io/astranautical/mccourse/block/ModBlocks.java
+++ src/main/java/io/astranautical/mccourse/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package io.astranautical.mccourse.block;
 
 import io.astranautical.mccourse.MCCourseMod;
+import io.astranautical.mccourse.block.custom.SpeedyBlock;
 import io.astranautical.mccourse.item.ModItemGroups;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -14,17 +15,25 @@ import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
+    // ORICHALCUM BLOCK
     public static final Block ORICHALCUM_BLOCK = registerBlock("orichalcum_block",
             new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroups.COURSE);
 
+    // ORICHALCUM ORE
     public static final Block ORICHALCUM_ORE = registerBlock("orichalcum_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroups.COURSE);
 
+    // DEEPSLATE ORICHALCUM ORE
     public static final Block DEEPSLATE_ORICHALCUM_ORE = registerBlock("deepslate_orichalcum_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool()), ModItemGroups.COURSE);
 
+    // RAW ORICHALCUM BLOCK
     public static final Block RAW_ORICHALCUM_BLOCK = registerBlock("raw_orichalcum_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool()), ModItemGroups.COURSE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool()), ModItemGroups.COURSE);
+
+    // SPEEDY BLOCK
+    public static final Block SPEEDY_BLOCK = registerBlock("speedy_block",
+            new SpeedyBlock(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool()), ModItemGroups.COURSE);
 
 
     // Adds functionality to add custom Blocks to the game.
